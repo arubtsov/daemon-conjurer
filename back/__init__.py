b9a91da81d13f0bd2883d67e0e26557d22c3065b
arubtsov <arseniy.rubtsov@gmail.com>
2020-09-15 02:24:52 +0300

    change data logging

--- back/__init__.py
+++ back/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/hooks/', methods=['POST'])
 def foo():
    data = json.loads(request.data)
-   print("New commit by: {}".format(data['commits'][0]['author']['name']))
+   print("New commit info: {}".format(data))
    return "OK"
 
 if __name__ == '__main__':
