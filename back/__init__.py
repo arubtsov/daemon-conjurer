6c1c85e37a65c4552e70e6cd80c15d5e11f845e6
arubtsov <arseniy.rubtsov@gmail.com>
2020-09-15 21:08:43 +0300

    add simple state and show it in root endpoint

--- back/__init__.py
+++ back/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask, request
 import json
 
 app = Flask(__name__)
+events = []
 
 @app.route('/hooks/', methods=['POST'])
 def foo():
    data = json.loads(request.data)
+   events.append(data)
    print("New commit info: {}".format(data))
    return "OK"
+
+@app.route('/', methods=['GET'])
+def show():
+    return "{}".format(events)
 
 if __name__ == '__main__':
    app.run()
